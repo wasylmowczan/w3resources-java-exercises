914289caea5045cc8ab5ae0cb806310c8165fde8
Wasyl Mowczan <mowczanwasyl@gmail.com>
2020-10-26 20:38:11 +0100

    Init commit First two exercises from basic module

---
+++ src/com/w3resource/basic_part_I/Exercise_1.java
@@ -0,0 +1,8 @@
+package com.w3resource.basic_part_I;
+
+public class Exercise_1 {
+    //Write a java program to print "Hello" on screen and then print your name on separate line.
+    public static void main(String[] args) {
+        System.out.println("Hello\nWasyl Mowczan");
+    }
+}
