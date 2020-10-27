bf61fbb5d7f1aa0dafe4f52c3b1b5d4fb1963df4
Wasyl Mowczan <mowczanwasyl@gmail.com>
2020-10-27 20:47:51 +0100

    Task 3,4 Task with stdout

--- src/com/w3resource/basic_part_I/Exercise_1.java
+++ src/com/w3resource/basic_part_I/Exercise_1.java
@@ -1,7 +1,10 @@
 package com.w3resource.basic_part_I;
 
 public class Exercise_1 {
-    //Write a java program to print "Hello" on screen and then print your name on separate line.
+    /*
+    Write a java program to print "Hello" on screen and then print your name on separate line.
+     */
+
     public static void main(String[] args) {
         System.out.println("Hello\nWasyl Mowczan");
     }
---
+++ src/com/w3resource/basic_part_I/Exercise_3.java
@@ -0,0 +1,20 @@
+package com.w3resource.basic_part_I;
+
+import java.util.Scanner;
+
+public class Exercise_3 {
+    /*
+    Write a Java program to divide two numbers and print on the screen.
+     */
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Input the first number: ");
+        int a = input.nextInt();
+        System.out.print("Input the second number: ");
+        int b = input.nextInt();
+        int d = (a/b);
+        System.out.println();
+        System.out.println("The division of a and b is:" +d);
+    }
+}
