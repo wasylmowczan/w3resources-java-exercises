eafdf7914fc210a1bedd11b6e36a317fc9449604
Wasyl Mowczan <mowczanwasyl@gmail.com>
2020-12-07 09:09:54 +0100

    Add next 4 exercises
    Added solution for tasks 19,20,21,22 in Basic Part-1 section

---
+++ src/com/w3resource/basic_part_I/Exercise_22.java
@@ -0,0 +1,20 @@
+package com.w3resource.basic_part_I;
+
+import java.util.Scanner;
+
+public class Exercise_22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long binaryNumber, decimalNumber = 0, j = 1, remainder;
+        System.out.print("Input a binary number: ");
+        binaryNumber = sc.nextLong();
+
+        while (binaryNumber != 0) {
+            remainder = binaryNumber % 10;
+            decimalNumber = decimalNumber + remainder * j;
+            j = j * 2;
+            binaryNumber = binaryNumber / 10;
+        }
+        System.out.println("Decimal Number: " + decimalNumber);
+    }
+}
